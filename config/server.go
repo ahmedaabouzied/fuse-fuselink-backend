4b017cd7c929e222257fe333f8f1aa1223b07328
Ahmed Abouzied <email@aabouzied.com>
2020-12-25 23:10:04 +0200

    feat: Update server config
    * Add user repo to repositories.
    * Add auth handler.
    * Add user usecase.
    
    Signed-off-by: Ahmed Abouzied <email@aabouzied.com>

--- config/server.go
+++ config/server.go
@@ -2,17 +2,22 @@
 package config
 
 import (
+	"bitbucket.org/MoMoLab-dev/fuse.link-backend/auth"
+	"bitbucket.org/MoMoLab-dev/fuse.link-backend/user"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 // Server represents the Server configuration structure
 type Server struct {
-	Env                  string                    // application environment
-	Port                 string                    // application port
-	HostName             string                    // application host name
-	DB                   *mongo.Client             // MongoDB cluster client
-	Repos                *Repositories             // Repositories
+	Env         string           // application environment
+	Port        string           // application port
+	HostName    string           // application host name
+	DB          *mongo.Client    // MongoDB cluster client
+	Repos       *Repositories    // Repositories
+	AuthHandler auth.AuthHandler // Authentication Handler
+	UserUsecase user.UserUsecase
 }
 
 type Repositories struct {
+	UserRepository user.UserRepository
 }
