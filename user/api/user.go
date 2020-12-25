e39917ec16ce9ebf04d903bfbe4ca7d055e0b722
Ahmed Abouzied <email@aabouzied.com>
2020-12-25 23:13:48 +0200

    feat: Implement create user endpoint
    Signed-off-by: Ahmed Abouzied <email@aabouzied.com>

---
+++ user/api/user.go
@@ -0,0 +1,48 @@
+package api
+
+import (
+	"bitbucket.org/MoMoLab-dev/fuse.link-backend/config"
+	"bitbucket.org/MoMoLab-dev/fuse.link-backend/entities"
+	"bitbucket.org/MoMoLab-dev/fuse.link-backend/user"
+	"bitbucket.org/MoMoLab-dev/fuse.link-backend/utils"
+	"context"
+	"github.com/gin-gonic/gin"
+	errors "github.com/pkg/errors"
+	"net/http"
+)
+
+type UserAPIHandler struct {
+	userUsecase user.UserUsecase
+}
+
+func NewUserAPI(server *config.Server) *UserAPIHandler {
+	return &UserAPIHandler{
+		userUsecase: server.UserUsecase,
+	}
+}
+
+// CreateUser creates a new user record with a username and
+// empty social profile records.
+func (h *UserAPIHandler) CreateUser(c *gin.Context) {
+	ctx := context.Background()
+	cognitoID, ok := c.Get(entities.UserIDContextKey)
+	if !ok {
+		utils.HandleAPIError(c, errors.Wrap(errors.New("token does not encode a cognito user ID"), utils.ErrorInvalidAuthorizationHeader.Error()))
+	}
+	var userReq entities.CreateUserRequest
+	err := c.BindJSON(&userReq)
+	if err != nil {
+		utils.HandleAPIError(c, errors.Wrap(err, utils.ErrorParsingRequest.Error()))
+	}
+	user := &entities.User{
+		Username:      userReq.Username,
+		CognitoUserID: cognitoID.(string),
+	}
+	createdUser, err := h.userUsecase.CreateUser(ctx, user)
+	if err != nil {
+		err = errors.Wrap(err, utils.ErrorCreatingUser.Error())
+	}
+	c.JSON(http.StatusCreated, gin.H{
+		"user": createdUser,
+	})
+}
