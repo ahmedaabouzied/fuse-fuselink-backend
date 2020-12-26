95e74093e054d832c73c5d12f6f030941e101f49
Ahmed Abouzied <email@aabouzied.com>
2020-12-27 01:39:56 +0200

    test: Test setting user ID to request context
    Signed-off-by: Ahmed Abouzied <email@aabouzied.com>

--- routes_test.go
+++ routes_test.go
@@ -8,6 +8,7 @@ import (
 
 	mockauth "bitbucket.org/MoMoLab-dev/fuse.link-backend/auth/mocks"
 	"bitbucket.org/MoMoLab-dev/fuse.link-backend/config"
+	"bitbucket.org/MoMoLab-dev/fuse.link-backend/entities"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 )
@@ -57,5 +58,40 @@ func TestAuthMiddleare(t *testing.T) {
 		rr := httptest.NewRecorder()
 		router.ServeHTTP(rr, request)
 		mockAuthHandler.AssertCalled(t, "ParseAuthToken", trimmedAuthToken)
+	})
+	t.Run("sets user cognito id to context value", func(t *testing.T) {
+		// setup a mock auth handler and server config
+		mockAuthHandler := &mockauth.AuthHandler{}
+		serverConfig := &config.Server{
+			AuthHandler: mockAuthHandler,
+		}
+
+		gin.SetMode(gin.TestMode)
+
+		// Return error on parsing token
+		mockAuthToken := "Bearer MOCK_AUTH_TOKEN"
+		trimmedAuthToken := "MOCK_AUTH_TOKEN"
+		parsedToken := map[string]interface{}{
+			"cognito:username": "test_username",
+		}
+		mockAuthHandler.On("ParseAuthToken", trimmedAuthToken).Return(parsedToken, nil)
+		testHandler := func(c *gin.Context) {
+			userID, ok := c.Get(entities.UserIDContextKey)
+			if !ok {
+				c.AbortWithStatus(http.StatusUnauthorized)
+			}
+			if userID != "test_username" {
+				c.AbortWithStatus(http.StatusUnauthorized)
+			}
+			c.AbortWithStatus(http.StatusOK)
+		}
+		// create a gin router with middlware
+		router := gin.Default()
+		router.GET("/test", authMiddleware(serverConfig), testHandler)
+		request := httptest.NewRequest("GET", "/test", nil)
+		request.Header.Add("Authorization", mockAuthToken)
+		rr := httptest.NewRecorder()
+		router.ServeHTTP(rr, request)
+		assert.Equal(t, http.StatusOK, rr.Code)
 	})
 }
